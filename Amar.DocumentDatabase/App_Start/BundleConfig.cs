55ff0773ba8dc61d6aec0eea84d96bbd008201a0
Amar Hajro <amarhajro@gmail.com>
2017-02-09 22:51:40 +0100

    Managed fof bootstrap and site.css to work together. Added style to header.

--- Amar.DocumentDatabase/App_Start/BundleConfig.cs
+++ Amar.DocumentDatabase/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@ namespace Amar.DocumentDatabase
     {
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
-        {
+        {                 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -17,14 +17,15 @@ namespace Amar.DocumentDatabase
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
-
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap*"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css"));    
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css"));
+   
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));            
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
